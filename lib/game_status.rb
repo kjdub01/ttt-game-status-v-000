996a9cde16e4ad201ea81b0d6a307b3a65c8d5c5
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-17 02:08:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,5 +25,5 @@ WIN_COMBINATIONS = [
   position_2 = board[win_index_2] 
   position_3 = board[win_index_3] 
   
-  f position_1 == "X" && position_2 == "X" && position_3 == "X"
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
   end
