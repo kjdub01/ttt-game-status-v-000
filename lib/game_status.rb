9ae744ba3564f234893e4384b1af410afcfc6a8f
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-17 01:54:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,11 +3,6 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-def won?(board)
-   WIN_COMBINATIONS.detect do |
-  end
-  
-  
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0, 1, 2],
@@ -20,3 +15,6 @@ WIN_COMBINATIONS = [
   [2, 4, 6]
   ]
   
+  def won?(board)
+   WIN_COMBINATIONS.detect do |
+  end
