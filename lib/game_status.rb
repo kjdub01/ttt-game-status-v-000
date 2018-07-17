7f720cde72b975db0736c2c2144d0684b64bc88c
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-17 02:34:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,9 @@ WIN_COMBINATIONS = [
   
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combo|
-    position_1 = board[win_combo[0]
-    position_2 = board[win_combo[1]
-    position_3 = board[win_combo[2]
+    position_1 = board[win_combo[0]]
+    position_2 = board[win_combo[1]]
+    position_3 = board[win_combo[2]]
     
     position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
   end
