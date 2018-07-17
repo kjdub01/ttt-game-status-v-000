c29ac2b71d1388d3326a6f37f888cdf65c71e4ff
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-17 02:33:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,6 @@ WIN_COMBINATIONS = [
   
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combo|
-    
     position_1 = board[win_combo[0]]
     position_2 = board[win_combo[1]]
     position_3 = board[win_combo[2]
